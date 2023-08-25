fa1c529b20709608b55887acc1968e70aae3cbba
behldevansh <heydevanshbehl@gmail.com>
2023-08-25 23:39:26 +0530

    added changes in cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ export default function Card() {
         <div className="bg-white py-6 sm:py-8 lg:py-12">
   <div className="mx-auto max-w-screen-xl px-4 md:px-8">
     <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl xl:mb-12">
-      Information
+    Our Services
     </h2>
     <div className="mb-6 grid gap-6 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 lg:gap-8">
       <div className="flex flex-col overflow-hidden rounded-lg border sm:mt-8">
@@ -13,10 +13,10 @@ export default function Card() {
         <div className="flex flex-1 flex-col p-6 pt-8">
           <div className="mb-12">
             <div className="mb-2 text-center text-2xl font-bold text-gray-800">
-            Our Mission
+            3D Printing Parts and Modular Automatic Space Assembly
             </div>
             <p className="mb-8 px-8 text-center text-gray-500">
-            We're committed to making the universe's boundless beauty and complexity come alive in the digital realm.
+            3D printing allows for the on-demand production of components using raw materials available in space or brought from Earth.
 
             </p>
             <div className="space-y-4">
@@ -42,7 +42,7 @@ export default function Card() {
                     className="text-gray-500"
                   />
                 </svg>
-                <span className="text-gray-600">1.000 MB file storage</span>
+                <span className="text-gray-600">Reduced Payload Mass</span>
               </div>
               <div className="flex items-center gap-2">
                 <svg
@@ -67,7 +67,7 @@ export default function Card() {
                   />
                 </svg>
                 <span className="text-gray-600">
-                  2.000 MB bandwidth per month
+                Customization
                 </span>
               </div>
               <div className="flex items-center gap-2">
@@ -92,7 +92,7 @@ export default function Card() {
                     className="text-gray-500"
                   />
                 </svg>
-                <span className="text-gray-600">200 tasks per month</span>
+                <span className="text-gray-600">Resource Utilization</span>
               </div>
               <div className="flex items-center gap-2">
                 <svg
@@ -101,46 +101,46 @@ export default function Card() {
                   fill="none"
                   viewBox="0 0 16 16"
                 >
-                  <circle
+                  {/* <circle
                     cx={8}
                     cy={8}
                     r={8}
                     fill="currentColor"
                     className="text-gray-300"
-                  />
-                  <circle
+                  /> */}
+                  {/* <circle
                     cx={8}
                     cy={8}
                     r={3}
                     fill="currentColor"
                     className="text-gray-500"
-                  />
+                  /> */}
                 </svg>
-                <span className="text-gray-600">Comunity support</span>
+                {/* <span className="text-gray-600">Comunity support</span> */}
               </div>
             </div>
           </div>
           <div className="mt-auto">
-            <a
+            {/* <a
               href="#"
               className="block rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base"
             >
               $0 / Free
-            </a>
+            </a> */}
           </div>
         </div>
       </div>
       <div className="flex flex-col overflow-hidden rounded-lg border-2 border-indigo-500">
         <div className="bg-indigo-500 py-2 text-center text-sm font-semibold uppercase tracking-widest text-white">
-          Popular choise
+          Popular choice
         </div>
         <div className="flex flex-1 flex-col p-6 pt-8">
           <div className="mb-12">
             <div className="mb-2 text-center text-2xl font-bold text-gray-800">
-              Team
+            Space based data centres
             </div>
             <p className="mx-auto mb-8 px-8 text-center text-gray-500">
-              Avanced feaures for Individuals and organizations
+              Avanced feaures for organizations for high safety and extremely fast data transmission.
             </p>
             <div className="space-y-4">
               <div className="flex items-center gap-2">
@@ -165,7 +165,7 @@ export default function Card() {
                     className="text-gray-500"
                   />
                 </svg>
-                <span className="text-gray-600">Unlimited file storage</span>
+                <span className="text-gray-600">Maintenance and Upgrades</span>
               </div>
               <div className="flex items-center gap-2">
                 <svg
@@ -189,7 +189,7 @@ export default function Card() {
                     className="text-gray-500"
                   />
                 </svg>
-                <span className="text-gray-600">10 GB bandwidth per month</span>
+                <span className="text-gray-600">Linking satellites for long range transmission</span>
               </div>
               <div className="flex items-center gap-2">
                 <svg
@@ -213,10 +213,10 @@ export default function Card() {
                     className="text-gray-500"
                   />
                 </svg>
-                <span className="text-gray-600">10.000 tasks per month</span>
+                <span className="text-gray-600">Data travel at light speed</span>
               </div>
               <div className="flex items-center gap-2">
-                <svg
+                {/* <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-4 w-4 shrink-0"
                   fill="none"
@@ -236,11 +236,11 @@ export default function Card() {
                     fill="currentColor"
                     className="text-gray-500"
                   />
-                </svg>
-                <span className="text-gray-600">Email support</span>
+                </svg> */}
+                {/* <span className="text-gray-600">Data travel at light speed</span> */}
               </div>
               <div className="flex items-center gap-2">
-                <svg
+                {/* <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-4 w-4 shrink-0"
                   fill="none"
@@ -260,18 +260,18 @@ export default function Card() {
                     fill="currentColor"
                     className="text-gray-500"
                   />
-                </svg>
-                <span className="text-gray-600">100 Webhooks</span>
+                </svg> */}
+                {/* <span className="text-gray-600">10 Webhooks</span> */}
               </div>
             </div>
           </div>
           <div className="mt-auto">
-            <a
+            {/* <a
               href="#"
               className="block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base"
             >
               $19
-            </a>
+            </a> */}
           </div>
         </div>
       </div>
@@ -280,7 +280,7 @@ export default function Card() {
         <div className="flex flex-1 flex-col p-6 pt-8">
           <div className="mb-12">
             <div className="mb-2 text-center text-2xl font-bold text-gray-800">
-              Enterprise
+            Space Advertising
             </div>
             <p className="mx-auto mb-8 px-8 text-center text-gray-500">
               Maximum performace for organisations
